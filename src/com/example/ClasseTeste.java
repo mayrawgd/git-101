102acac3a4b816962d98524e33b0095e0b763b10
mayrawgd <mayrawgd@gmail.com>
2016-09-15 11:38:19 -0300

    primeiro commit

--- src/com/example/ClasseTeste.java
+++ src/com/example/ClasseTeste.java
@@ -1,9 +1,28 @@
 package com.example;
 
+import com.sun.media.sound.SimpleSoundbank;
+
 public class ClasseTeste {
+	
+	static ClasseTeste ct = new ClasseTeste ();
 	
 	public static void main(String[] args) {
 		System.out.println("Main method");
+		ct.soma();
+	
 	}
+	public int soma(){
+		
+		int a=2;
+		int b=3;
+		int soma = a+b;
+		
+		System.out.println(soma);
+		
+		return soma;
+		
+	}
+	
+	
 
 }
